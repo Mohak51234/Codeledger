26f5ec78e9c7c1a8a28ae85b108b604ff9ac7899
Mohak <143473709+Mohak51234@users.noreply.github.com>
2026-07-18 19:09:10 +0530

    Fix env var name mismatch

--- server/src/config/db.js
+++ server/src/config/db.js
@@ -3,7 +3,7 @@ import mongoose from 'mongoose';
 export async function connectDB() {
     const uri = process.env.MONGODB_URI;
     if (!uri) {
-        throw new Error('MONGO_URI is not set');
+        throw new Error('MONGODB_URI is not set');
     }
     await mongoose.connect(uri);
     console.log('[db] connected to MongoDB');
